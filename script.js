5b613c74d7052a8bad9a67dc7cc47c6a90fe2ab2
Kevin Dinh <kevinqdinh25@gmail.com>
2021-01-06 13:37:46 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    div = document.getElementById('sideNav');
+    div.style.display = "block";
+}
+
+function hideMenu() {
+    div = document.getElementById('sideNav');
+    div.style.display = "none";
+}
