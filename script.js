362cffdd42cf490c7da647d5461ea453620a1933
Kevin Dinh <50538800+kevindinhh@users.noreply.github.com>
2023-11-15 23:14:46 +1100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-    div = document.getElementById('sideNav');
-    div.style.display = "block";
-}
-
-function hideMenu() {
-    div = document.getElementById('sideNav');
-    div.style.display = "none";
-}
